40e81296c5fa1352a012805147d395b6dbf38e36
NelsonRodriguez7 <nelsonpay2020@hotmail.com>
2022-12-06 12:16:53 -0500

    corregido una ruta que estaba mal

--- controllers/login_controllers.php
+++ controllers/login_controllers.php
@@ -4,7 +4,7 @@
             $title = "Login | DS 7";
             require_once("views/template/header.php");
             require_once("views/template/nav.php");
-            require_once("views/formularios.php/login.php");
+            require_once("views/formularios/login.php");
             require_once("views/template/footer.php");
         }
     }
