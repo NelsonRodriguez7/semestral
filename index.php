94a9954175db8d8cba4b14c1d30ca355cfbca39e
NelsonRodriguez7 <nelsonpay2020@hotmail.com>
2022-12-07 10:17:52 -0500

    todos los modelos agregados

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     require_once("controllers/registrar_controllers.php");
     require_once("controllers/menu_controllers.php");
     require_once("controllers/mensaje_controllers.php");
+    require_once("controllers/pagos_paypal_controllers.php");
     require_once("utils/seg.php");
     if (count($_GET)==0)
         call_user_func("principal_controllers::index");
