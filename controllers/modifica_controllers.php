3829b26f9a2ff1ef44ad4de507017794369f7fe1
AnthonyGonzalez7 <anthony.gonzalez7@utp.ac.pa>
2022-12-08 21:39:30 -0500

    Modificar datos de usuario terminado

--- controllers/modifica_controllers.php
+++ controllers/modifica_controllers.php
@@ -1,11 +1,56 @@
 <?php 
+require_once("utils/seg.php");
+require_once("utils/utils.php");
+require_once("models/usuarios_models.php");
     class modifica_controllers{
         public static function modifica(){
+            if (!isset($_SESSION["id_usuario"])) {
+                header("location:" . "index.php?c=" . seg::codificar("mensaje") . "&m=" . seg::codificar("index") . "&msg=Notiene acceso a esta pantalla, debe acceder para continuar");
+                exit();
+            }
+            $obj = new usuarios();
+            $obj->setId($_SESSION["id_usuario"]);
+            $resultados = $obj->ver_mis_datos();
             $title = "Modifica Datos | DS 7";
             require_once("views/template/header.php");
             require_once("views/template/nav.php");
             require_once("views/formularios/modificadatos.php");
             require_once("views/template/footer.php");
         }
+
+        public static function modificar(){
+            if ($_POST) {
+                if (!isset($_POST["token"]) ||  !seg::validaSesion($_POST["token"])) {
+                    echo "Acceso restringido";
+                    exit();
+                }
+
+                
+                $nombre_contacto = $_POST["txtNombre"];
+                $nombre_empresa = $_POST["txtNombreEmpresa"]; 
+                $logo_restaurante = utils::subir_archivo($_FILES["imgLogo"]["tmp_name"],$_FILES["imgLogo"]["name"], "uploads"); 
+                $imagen_fondo = utils::subir_archivo($_FILES["imgFondo"]["tmp_name"],$_FILES["imgFondo"]["name"], "uploads");                
+                $correo = filter_var($_POST["txtCorreo"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $cuenta_paypal = filter_var($_POST["txtCuentaPaypal"], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $nombre_contacto = filter_var($nombre_contacto, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $nombre_empresa = filter_var($nombre_empresa, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                $obj = new usuarios();
+                $obj->setId($_SESSION["id_usuario"]);
+                $obj->setCorreo($correo);
+                $obj->setNombre_contacto($nombre_contacto);
+                $obj->setNombre_restaurante($nombre_empresa);
+                $obj->setCuenta_paypal($cuenta_paypal);
+                $obj->setLogo_empresa($logo_restaurante);
+                $obj->setImagen_fondo($imagen_fondo);
+    
+                $resultados = $obj->actualizar_datos_generales();
+                if (isset($resultados)) {
+                    header("location:" . "index.php?c=" . seg::codificar("mensaje") . "&m=" . seg::codificar("index") . "&msg=Se ha modificado satisfactoriamente su cuenta <br><br>Gracias");
+                } else
+                    header("location:" . "index.php?c=" . seg::codificar("mensaje") . "&m=" . seg::codificar("index") . "&msg=No se pudo actualizar, intentelo nuevamente!");
+                
+            }
+        }
+
     }
 ?>
--- views/formularios/modificadatos.php
+++ views/formularios/modificadatos.php
@@ -11,36 +11,31 @@
                 </div>  
                 <div class="row justify-content-center">
                     <div class="col-12">
-                        <form action="#">
+                        <form method="POST" enctype="multipart/form-data" action="<?php echo "index.php?c=" . seg::codificar("modifica") . "&m=" . seg::codificar("modificar") ?>">
                             <div class="booking-wrap d-flex justify-content-between align-items-center">
                                 <div>
                                     <h5>Usuario <FONT COLOR="red">(*) </FONT></h5>
-                                    <input type="text" name="modificausuario" placeholder="Ejemplo: Jperez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Jperez'" required="" class="single-input">
+                                    <input type="text" name="modificausuario" placeholder="Ejemplo: Jperez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Jperez'" required="" class="single-input" value="<?php echo isset($resultados["usuario"]) ? $resultados["usuario"] : "" ?>" disabled>
                                     <h5>Nombre de contacto</h5>
-                                    <input type="text" name="modificanombre" placeholder="Ejemplo: Juan Perez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Juan Perez'" required="" class="single-input">
+                                    <input type="text" name="txtNombre" placeholder="Ejemplo: Juan Perez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Juan Perez'" required="" class="single-input" value="<?php echo isset($resultados["nombre_contacto"]) ? $resultados["nombre_contacto"] : "" ?>">
                                     <h5>Cuenta de PayPal <FONT COLOR="red">(*) </FONT></h5>
-                                    <input type="email" name="modificacorreopaypal" placeholder="Ejemplo: jperezxd@dominio.com" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: jperezxd@dominio.com'" required="" class="single-input">
-                                    <h5>Imagen de Fondo</h5>
-                                    <input type="email" name="modificaurllogouser" placeholder="logo de imagen de fondo (800x800px)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'logo de imagen de fondo (800x800px)'" required="" class="single-input">
-                                    <form action=“destino.php" method="post" enctype="multipart/form-data">
-                                    <input class="form-control" type="file" id="formFile" name="foto" accept="image/*">
+                                    <input type="email" name="txtCuentaPaypal" placeholder="Ejemplo: jperezxd@dominio.com" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: jperezxd@dominio.com'" required="" class="single-input" value="<?php echo isset($resultados["cuenta_paypal"]) ? $resultados["cuenta_paypal"] : "" ?>">
+                                    <h5>Imagen de Fondo</h5> 
+                                    <input class="form-control" type="file" id="formFile" name="imgFondo" accept="image/*">
                                     <BR>
-                                    <form>
+
 
                                     <p><FONT COLOR="red">(*) Obligatorio</FONT></p>
                                 </div>
                                 <div>
                                     <h5>Correo electronico <FONT COLOR="red">(*) </FONT></h5>
-                                    <input type="email" name="modificacorreouser" placeholder="Ejemplo: jperezxd@dominio.com" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: jperezxd@dominio.com'" required="" class="single-input">
+                                    <input type="email" name="txtCorreo" placeholder="Ejemplo: jperezxd@dominio.com" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: jperezxd@dominio.com'" required="" class="single-input" value="<?php echo isset($resultados["correo"]) ? $resultados["correo"] : "" ?>">
                                     <h5>Nombre Restaurante</h5>
-                                    <input type="text" name="modificarestaurante" placeholder="Ejemplo: Restaurante JPerez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Jperez'" required="" class="single-input">
+                                    <input type="text" name="txtNombreEmpresa" placeholder="Ejemplo: Restaurante JPerez" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Ejemplo: Jperez'" required="" class="single-input" value="<?php echo isset($resultados["nombre_restaurante"]) ? $resultados["nombre_restaurante"] : "" ?>">
                                     <h5>Logo de Restaurante</h5>
-                                    <input type="email" name="modificaurllogorest" placeholder="url del logo de imagen de fondo (800x800px)" onfocus="this.placeholder = ''" onblur="this.placeholder = 'url del logo de imagen de fondo (800x800px)'" required="" class="single-input">
-                                    <form action=“destino.php" method="post" enctype="multipart/form-data">
-                                    <input class="form-control" type="file" id="formFile" name="foto" accept="image/*">
-                                    <form>
+                                    <input class="form-control" type="file" id="formFile" name="imgLogo" accept="image/*">
                                 </div>
-                                
+                                <input type="hidden" name="token" value="<?php echo seg::getToken() ?>">
                                 <!-- Single Select Box -->
                                 <div class="single-select-box mb-30">
                                     <button class="btn select-btn">Modificar</button>
